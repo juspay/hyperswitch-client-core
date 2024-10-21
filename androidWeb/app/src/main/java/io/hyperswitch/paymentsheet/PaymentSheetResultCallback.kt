f9678aa9b81500b6cf27377814bd703fe792199c
ChiragKV-Juspay <chirag.kv@juspay.in>
2024-10-21 16:24:50 +0530

    refactor: removed androidWeb directory

--- androidWeb/app/src/main/java/io/hyperswitch/paymentsheet/PaymentSheetResultCallback.kt
+++
@@ -1,8 +0,0 @@
-package io.hyperswitch.paymentsheet
-
-/**
- * Callback that is invoked when a [PaymentSheetResult] is available.
- */
-fun interface PaymentSheetResultCallback {
-    fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult)
-}
