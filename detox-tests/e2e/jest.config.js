12455a082bca485132319dca6e04ca9560e38032
Sanskar Atrey <sanskaratrey22@gmail.com>
2024-12-17 14:12:16 +0530

    chore: babel config

--- detox-tests/e2e/jest.config.js
+++ detox-tests/e2e/jest.config.js
@@ -10,4 +10,8 @@ module.exports = {
   reporters: ['detox/runners/jest/reporter'],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   verbose: true,
+  transform: {
+    // Add a transformer for `.bs.js` files
+    '\\.bs\\.js$': 'babel-jest', // or specify a custom transformer if needed
+  },
 };
