3088752165fba57232f833a525ba9a00c8b03e74
Sanskar Atrey <sanskaratrey22@gmail.com>
2024-12-17 12:17:41 +0530

    chore: react native detox e2e fram
    ework setup and card flow test added

--- .detoxrc.js
+++ .detoxrc.js
@@ -3,7 +3,7 @@ module.exports = {
   testRunner: {
     args: {
       $0: 'jest',
-      config: 'e2e/jest.config.js',
+      config: 'detox-tests/e2e/jest.config.js',
     },
     jest: {
       setupTimeout: 120000,
--- detox-tests/e2e/card-flow-e2e.test.ts
+++ detox-tests/e2e/card-flow-e2e.test.ts
@@ -2,40 +2,19 @@ const cardNumberInputTestId = 'CardNumberInputTestId';
 const cvcInputTestId = 'CVCInputTestId';
 const expiryInputTestId = 'ExpiryInputTestId';
 const payButtonTestId = 'Pay';
+
+import { device } from "detox"
 describe('Example', () => {
   jest.retryTimes(6);
   beforeAll(async () => {
     await device.launchApp({
-      launchArgs: {detoxEnableSynchronization: 1},
+      launchArgs: { detoxEnableSynchronization: 1 },
       newInstance: true,
     });
     await device.enableSynchronization();
   });
 
-  // testing 5
-  // testing 2
-  // test 20
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  // testing 3
-  //
-  //
-  //
-  //
-  // beforeEach(async () => {
-  //   await device.reloadReactNative();
-  // });
-
-  it('should have welcome screen', async () => {
-    // await expect(element(by.id('welcome'))).toBeVisible();
-    // await expect(element(by.text('Launch Payment Sheet'))).toBeVisible();
-
+  it('demo app should load successfully', async () => {
     await waitFor(element(by.text('Launch Payment Sheet')))
       .toBeVisible()
       .withTimeout(10000);
@@ -44,9 +23,6 @@ describe('Example', () => {
     await waitFor(element(by.text('Test Mode')))
       .toBeVisible()
       .withTimeout(10000);
-
-    // await element(by.text('MM / YY')).typeText('04/24');
-    // await element(by.text('CVC')).typeText('424');
   });
 
   it('should enter card no', async () => {
@@ -62,9 +38,6 @@ describe('Example', () => {
 
     await element(by.id(cardNumberInputTestId)).clearText();
     await element(by.id(cardNumberInputTestId)).replaceText('4242424242424242');
-    // await expect(element(by.id(cardNumberInputTestId))).toHaveText(
-    //   '4242424242424242',
-    // );
 
     await waitFor(element(by.id(expiryInputTestId))).toExist();
     await waitFor(element(by.id(expiryInputTestId))).toBeVisible();
@@ -74,16 +47,13 @@ describe('Example', () => {
     await waitFor(element(by.id(cvcInputTestId))).toBeVisible();
     await element(by.id(cvcInputTestId)).typeText('123');
 
-    // await waitFor(element(by.id(payButtonTestId))).toExist();
+
     await waitFor(element(by.id(payButtonTestId)))
       .toBeVisible()
       .withTimeout(10000);
 
-    // await device.pressBack();
-    await element(by.id(payButtonTestId)).tap();
-    // await element(by.id(payButtonTestId)).tap();
 
-    // await element(by.id('CardNumberInput')).typeText('1234');
+    await element(by.id(payButtonTestId)).tap();
     await waitFor(element(by.text('succeeded')))
       .toBeVisible()
       .withTimeout(10000);
