0145a4296c0552a272e3e665b282334af1d90fbe
Sanskar Atrey <sanskaratrey22@gmail.com>
2024-12-26 16:11:10 +0530

    feat: added react native detox framework for e2e automation (#165)

---
+++ detox-tests/fixtures/Constants.ts
@@ -0,0 +1,8 @@
+type card = {
+    cardNumber: string,
+    expiryDate: string,
+    cvc: string,
+}
+
+export const visaSandboxCard = { cardNumber: "4242424242424242", expiryDate: "04/44", cvc: "123" }
+export const LAUNCH_PAYMENT_SHEET_BTN_TEXT = "Launch Payment Sheet"
