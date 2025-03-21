2cdffa8b9b99f8270e139c0e3ce737df2b2e6523
sh-iv-am <shivam.shashank@juspay.in>
2025-03-21 20:55:31 +0530

    chore: workflow update

--- reactNativeWeb/webpack.config.js
+++ reactNativeWeb/webpack.config.js
@@ -1,19 +1,20 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
-require('dotenv').config({ path: './.env' });
+require('dotenv').config({path: './.env'});
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const appDirectory = path.resolve(__dirname);
-const { presets, plugins } = require(`${appDirectory}/babel.config.js`);
+const {presets, plugins} = require(`${appDirectory}/babel.config.js`);
 const isDevelopment = process.env.NODE_ENV == 'development';
-const repoVersion = require("./version.json").version;
-const majorVersion = "v" + repoVersion.split(".")[0];
-const repoPublicPath = isDevelopment ? `` : `/mobile/${repoVersion}/mobile/${majorVersion}`;
+const repoVersion = require('./version.json').version;
+const majorVersion = 'v' + repoVersion.split('.')[0];
+const repoPublicPath = isDevelopment
+  ? ``
+  : `/mobile/${repoVersion}/mobile/${majorVersion}`;
 
-// console.log('dev mode --- >', isDevelopment);
 const compileNodeModules = [
   // Add every react-native package that needs compiling
   // 'react-native-gesture-handler',
