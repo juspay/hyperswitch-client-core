35dab5fbb5dd87a54250d7ebe85d6bd3f33cbc21
Pradeep Kumar <pradeep.kumar@juspay.in>
2025-05-12 17:14:16 +0530

    refactor: modified the detox test-case this commit is for testing purpose

--- detox-tests/e2e/card-flow-e2e.test.ts
+++ detox-tests/e2e/card-flow-e2e.test.ts
@@ -1,5 +1,5 @@
 import * as testIds from "../../src/utility/test/TestUtils.bs.js";
-import { device } from "detox"
+import { device, element, by, waitFor } from "detox"
 import { visaSandboxCard, LAUNCH_PAYMENT_SHEET_BTN_TEXT } from "../fixtures/Constants"
 import { waitForVisibility, typeTextInInput } from "../utils/DetoxHelpers"
 describe('card-flow-e2e-test', () => {
@@ -18,7 +18,11 @@ describe('card-flow-e2e-test', () => {
 
   it('payment sheet should open', async () => {
     await element(by.text(LAUNCH_PAYMENT_SHEET_BTN_TEXT)).tap();
-    await waitForVisibility(element(by.text('Test Mode')), 30000)
+    try {
+      await waitForVisibility(element(by.text('Test Mode')), 60000)
+    } catch (error) {
+      await waitForVisibility(element(by.id(testIds.cardNumberInputTestId)), 30000);
+    }
   })
 
   it('should enter details in card form', async () => {
@@ -43,14 +47,45 @@ describe('card-flow-e2e-test', () => {
   });
 
   it('should be able to succesfully complete card payment', async () => {
-    const payNowButton = await element(by.id(testIds.payButtonTestId))
-    await waitFor(payNowButton).toExist();
-    await waitForVisibility(payNowButton)
-    await payNowButton.tap();
+    try {
+      await element(by.id('card-form-scroll-view')).scrollTo('bottom');
+    } catch (error) {
+    }
+      let buttonFound = false;
+    
+    try {
+      const exactPayButton = element(by.text('Purchase ($2.00)'));
+      await waitFor(exactPayButton).toBeVisible().withTimeout(10000);
+      await exactPayButton.tap();
+      buttonFound = true;
+    } catch (error) {
+    }
+    
+    if (!buttonFound) {
+      try {
+        const payNowButton = element(by.id(testIds.payButtonTestId));
+        await waitFor(payNowButton).toExist().withTimeout(10000);
+        await waitForVisibility(payNowButton);
+        await payNowButton.tap();
+        buttonFound = true;
+      } catch (error) {
+      }
+    }
+    
+    if (!buttonFound) {
+      const payButtonTextMatcher = element(by.text(/Purchase|Pay|Continue/i));
+      await waitFor(payButtonTextMatcher).toBeVisible().withTimeout(20000);
+      await payButtonTextMatcher.tap();
+    }
 
-    if (device.getPlatform() === "ios")
-      await waitForVisibility(element(by.text('Payment complete')))
-    else
-      await waitForVisibility(element(by.text('succeeded')))
+    if (device.getPlatform() === "ios") {
+      await waitForVisibility(element(by.text('Payment complete')), 60000);
+    } else {
+      try {
+        await waitForVisibility(element(by.text('succeeded')), 30000);
+      } catch (error) {
+        await waitForVisibility(element(by.text(/success|completed|approved/i)), 30000);
+      }
+    }
   })
 });
