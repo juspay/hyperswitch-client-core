590ceadfb88e167399ef9d56bfb1441b2db79bd9
Kuntimaddi Manideep <kuntimaddi.manideep@juspay.in>
2026-04-10 01:16:42 +0530

    feat: integrate NFC card reading functionality with react-native-nfc-emv support

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,15 @@
+const path = require('path');
+
 module.exports = {
   assets: ['./assets/fonts/'],
   project: {
     android: {
       appName: 'demo-app',
+    },
+  },
+  dependencies: {
+    'react-native-nfc-emv': {
+      root: path.join(__dirname, '..', '..', '..', 'library', 'react-native-nfc-emv'),
     },
   },
 };
