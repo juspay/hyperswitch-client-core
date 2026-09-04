c487bb7241eb1d410cddb494ebb4e7cd5e7997b6
ChiragKV-Juspay <chirag.kv@juspay.in>
2026-09-04 17:24:24 +0530

    chore: revert incidental reformatting of index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-import {AppRegistry, Platform} from 'react-native';
-import NewApp, {HeadlessApp, runHeadlessTask} from './src/routes/Update';
-import {name as appName, headless} from './app.json';
+import { AppRegistry, Platform } from 'react-native';
+import NewApp, { HeadlessApp, runHeadlessTask } from './src/routes/Update';
+import { name as appName, headless } from './app.json';
 
 AppRegistry.registerComponent(appName, () => NewApp);
 
 if (Platform.OS === 'android') {
-  AppRegistry.registerHeadlessTask(
-    headless,
-    () => taskData => runHeadlessTask(taskData.props),
-  );
+    AppRegistry.registerHeadlessTask(headless, () => taskData => runHeadlessTask(taskData.props));
 } else if (Platform.OS === 'ios') {
-  AppRegistry.registerComponent(headless, () => HeadlessApp);
+    AppRegistry.registerComponent(headless, () => HeadlessApp);
 }
